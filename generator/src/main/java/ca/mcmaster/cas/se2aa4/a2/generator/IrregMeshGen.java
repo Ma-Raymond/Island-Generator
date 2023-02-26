eb526d29fbb3e0028b07af0899157cbf2b6feab6
Raymond Ma <raymond.ma03@outlook.com>
2023-02-26 04:53:48 -0500

    Fixed and Completed Generator for Voronoi

--- generator/src/main/java/ca/mcmaster/cas/se2aa4/a2/generator/IrregMeshGen.java
+++ generator/src/main/java/ca/mcmaster/cas/se2aa4/a2/generator/IrregMeshGen.java
@@ -1,10 +1,10 @@
 package ca.mcmaster.cas.se2aa4.a2.generator;
 import ca.mcmaster.cas.se2aa4.a2.io.Structs.*;
-import ca.mcmaster.cas.se2aa4.a2.io.Structs.Polygon;
-import ca.mcmaster.cas.se2aa4.a2.generator.MeshGen.*;
+
 
 import org.locationtech.jts.geom.*;
 //import org.locationtech.jts.geom.Polygon;
+import org.locationtech.jts.geom.Polygon;
 import org.locationtech.jts.operation.polygonize.Polygonizer;
 import org.locationtech.jts.triangulate.VoronoiDiagramBuilder;
 
@@ -16,31 +16,22 @@ import java.util.List;
 
 public class IrregMeshGen extends GeneralMesh {
     //THESE DATASETS ARE FOR IRREGULAR MESH
-    List<Coordinate> coords = new ArrayList<Coordinate>();
-    List<Coordinate[]> allPolygonVertices = new ArrayList<>();
-    Set<Vertex> verticesSet = new HashSet<>();
-    Set<Segment> segmentsSet = new HashSet<>();
-
     List<Vertex> siteList = new ArrayList<Vertex>();
-    List<Vertex> vertexList = new ArrayList<Vertex>();
-    List<Segment> segmentList = new ArrayList<Segment>();
-    DecimalFormat precision = new DecimalFormat("0.00");
-
 
     //user gets to decide the number of polygons. Will be taken from command line
     public void IrregularMesh() {
         VoronoiDiagramBuilder voronoi = new VoronoiDiagramBuilder();
         Envelope env = new Envelope(new Coordinate(0, 0), new Coordinate(WIDTH, HEIGHT));
-//        voronoi.setClipEnvelope(env);//given diagram size constraint
+        voronoi.setClipEnvelope(env);//given diagram size constraint
         PrecisionModel pointAcc = new PrecisionModel(0.01);
-
-//        voronoi.setTolerance(0.01); //may be redundant
+        voronoi.setTolerance(0.01); //may be redundant
 
         Random Val = new Random();
         Coordinate Coord = new Coordinate();
 
         //ARBITRARY VALUE TO BE CHANGED
         int points = 100;
+        List<Coordinate> coords = new ArrayList<Coordinate>();
 
         for (int i = 0; i < points; i++) {
             int xCoord = Val.nextInt(WIDTH);
@@ -55,7 +46,7 @@ public class IrregMeshGen extends GeneralMesh {
             coords.add(new Coordinate(xCoord,yCoord));
             //ADD SITES TO VERTEX LIST TO BE ABLE TO VISUALIZE
             siteList.add(newSite);
-            vertexList.add(newSite);
+//            vertexList.add(newSite);
         }
 
         voronoi.setSites(coords);
@@ -64,23 +55,30 @@ public class IrregMeshGen extends GeneralMesh {
         //allPolygons is like a finished puzzle, it contains all polygons
         Geometry allPolygons = voronoi.getDiagram(makePolygons);
         Polygonizer polygonizer = new Polygonizer();
-        polygonizer.add(allPolygons);
-        List<org.locationtech.jts.geom.Polygon> polys = new ArrayList<org.locationtech.jts.geom.Polygon>(polygonizer.getPolygons());
+        System.out.println("WTFFF"+allPolygons.getNumGeometries());
 
-        int numPoly = polys.size();
+        int numPoly = allPolygons.getNumGeometries();
         System.out.println(numPoly);
 
-        for (int i = 0; i < numPoly; i++) {
-            allPolygonVertices.add(polys.get(i).getCoordinates()); //will get vertices list of each individual polygon
-        }
-        System.out.println("HII"+allPolygonVertices.size());
+        List<Coordinate[]> allPolygonVertices = new ArrayList<>();
+        for (int i = 0; i < numPoly; i++) { //will get vertices list of each individual polygon
+            Geometry poly = allPolygons.getGeometryN(i);
+            allPolygonVertices.add(poly.getCoordinates());
 
-        for (Coordinate[] vertices : allPolygonVertices) {
+            // Centroid
+            Vertex centroid = Vertex.newBuilder().setX(poly.getCentroid().getX()).setY(poly.getCentroid().getY()).build();
+            centroidList.add(centroid);
+            vertexList.add(centroid);
+        }
+
+        System.out.println("HII" +allPolygonVertices.size());
+
+        for (Coordinate[] vertice : allPolygonVertices) {
             List<Integer> polySegments = new ArrayList<>();
 
-            for (int j = 0; j < vertices.length - 1; j++) {
-                Coordinate vert1 = vertices[j];
-                Coordinate vert2 = vertices[j + 1];
+            for (int j = 0; j < vertice.length - 1; j++) {
+                Coordinate vert1 = vertice[j];
+                Coordinate vert2 = vertice[j + 1];
                 double x1 = vert1.getX();
                 double y1 = vert1.getY();
                 double x2 = vert2.getX();
@@ -88,37 +86,38 @@ public class IrregMeshGen extends GeneralMesh {
                 Vertex v1 = Vertex.newBuilder().setX(Double.parseDouble(precision.format(x1))).setY(Double.parseDouble(precision.format(y1))).build();
                 Vertex v2 = Vertex.newBuilder().setX(Double.parseDouble(precision.format(x2))).setY(Double.parseDouble(precision.format(y2))).build();
 
-                List<Vertex> segments = new ArrayList<Vertex>(Arrays.asList(v1, v2));
-                for (Vertex v : segments) {
-                    if (!verticesSet.contains(v)) {
-                        verticesSet.add(v);
+                List<Vertex> segment = new ArrayList<Vertex>(Arrays.asList(v1, v2));
+                for (Vertex v : segment) {
+                    if (!vertices.contains(v)) {
+                        vertices.add(v);
                         vertexList.add(v);
                     }
                 }
 
                 Segment s = Segment.newBuilder().setV1Idx(vertexList.indexOf(v1)).setV2Idx(vertexList.indexOf(v2)).build();
-                if (!segmentsSet.contains(s)) {
-                    segmentsSet.add(s);
+                if (!segments.contains(s)) {
+                    segments.add(s);
                     segmentList.add(s);
                     polySegments.add(segmentList.indexOf(s));
                 }
             }
-            Polygon poly = Polygon.newBuilder().addAllSegmentIdxs(polySegments).build();
-            polygonList.add(poly);
-
+//            Polygon poly = Polygon.newBuilder().addAllSegmentIdxs(polySegments).build();
+//            polygonList.add(poly);
         }
 
     }
 
     public Mesh generate() {
         IrregularMesh();
-        System.out.println(segmentsSet.toString());
         //COLOUR SITE VERTEXES AND SEGMENTS
         for (Vertex v : vertexList) {
             int red = 255;
             int green = 0;
             int blue = 0;
             int alpha = 255;
+            if (!centroidList.contains(v)){
+                alpha = 0;
+            }
             colorVertex(v, red, green, blue, alpha);
 
         }

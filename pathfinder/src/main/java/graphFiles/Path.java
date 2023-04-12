d767251d08ae3f58359fe87613575d23c8dcb47e
Raymond Ma <raymond.ma03@outlook.com>
2023-04-11 22:30:18 -0400

    F2 - Interface Path

---
+++ pathfinder/src/main/java/graphFiles/Path.java
@@ -0,0 +1,10 @@
+package graphFiles;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+
+public interface Path {
+    public List<Integer> dijkstra(Graph graph,Integer node);
+
+}
